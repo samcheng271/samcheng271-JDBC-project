ec17b61fdbd3b582b73a3230e5cf48db4c02dd62
Samuel Cheng <samcheng271@gmail.com>
2025-01-30 00:57:40 +0000

    Save progress, working on message, test 1

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import Model.Account;
+import Model.Message;
 import Util.ConnectionUtil;
 
 /**
@@ -29,29 +30,72 @@ public class SocialMediaController {
         app.post("register", ctx -> {
             String Json = ctx.body();
             Account account = objectMapper.readValue(Json, Account.class);
+            String userN = account.getUsername();
             // Check if username and password are valid and if account already exists in database
-            if(account.getUsername().isEmpty() || account.getPassword().length() < 4 || accountExists(account.getUsername())) {
+            if(userN.isEmpty() || account.getPassword().length() < 4 || accountExists(userN)) {
                 ctx.status(400);
             } else {
-                addAccount(account.getUsername(), account.getPassword());
+                addAccount(userN, account.getPassword());
                 ctx.status(200);
-                account.setAccount_id(findID(account.getUsername()));
+                account.setAccount_id(findIDAccount(userN));
                 ctx.json(account);
-                // viewDatabase(account.getUsername());
+                // viewDatabase(userN);
             }
         });
 
+        // Login
+        app.post("login", ctx -> {
+            // json=ctx.body(); then object mapper then extract from account
+            String Json = ctx.body();
+            Account account = objectMapper.readValue(Json, Account.class);
+            String userN = account.getUsername();
+            // System.out.println(account.getPassword().equals(findPass(userN)) );
+            if(!accountExists(userN) || !(account.getPassword().equals(findPass(userN)))) {
+                ctx.status(401);
+            } else{
+                ctx.status(200);
+                account.setAccount_id(findIDAccount(userN));
+                ctx.json(account);
+            }
+        });
+
+        // create message
+        app.post("messages", ctx -> {
+            String Json = ctx.body();
+            Message message = objectMapper.readValue(Json, Message.class);
+            int userN = message.getPosted_by();
+            if(!MessengerExists(userN)) {
+                ctx.status(400);
+            } else {
+                // message must be less than 255 characters
+                if(message.getMessage_text().length() > 255 || message.getMessage_text().isEmpty()) {
+                    ctx.status(400);
+                }else {
+                    ctx.status(200);
+                    addMessage(message);
+                    System.out.println("in http msg: "+ findIDMessage(userN));
+                    viewDatabase();
+                    message.setMessage_id(findIDMessage(userN));
+                    ctx.json(message);
+                }
+            }
+        });
 
         return app;
     }
 
-    // /**
-    //  * This is an example handler for an example endpoint.
-    //  * @param context The Javalin Context object manages information about both the HTTP request and response.
-    //  */
-    // private void exampleHandler(Context context) {
-    //     context.json("sample text");
-    // }
+    // add message to database
+    private void addMessage(Message message) {
+        try(Connection conn = ConnectionUtil.getConnection()){
+            PreparedStatement ps = conn.prepareStatement("insert into message (message_text,posted_by) values (?,?);");
+            ps.setString(1, message.getMessage_text());
+            ps.setInt(2, message.getPosted_by());
+            // ps.set(2, message.getTime_posted_epoch());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     // add account to database
     private void addAccount(String username, String password) {
@@ -78,8 +122,7 @@ public class SocialMediaController {
         return false;
     }
 
-    
-    private int findID(String user) {
+    private int findIDAccount(String user) {
         try(Connection conn = ConnectionUtil.getConnection()){
             PreparedStatement ps = conn.prepareStatement("select account_id from account where username=?;");
             ps.setString(1, user);
@@ -94,15 +137,62 @@ public class SocialMediaController {
         }
         return -1;
     }
-
-    // view all values in database
-    private void viewDatabase(String user) {
+    
+    
+    private String findPass(String user) {
         try(Connection conn = ConnectionUtil.getConnection()){
-            PreparedStatement ps = conn.prepareStatement("select account_id from account where username=?;");
+            PreparedStatement ps = conn.prepareStatement("select password from account where username=?;");
             ps.setString(1, user);
             ResultSet rs = ps.executeQuery();
             while(rs.next()) {
-                System.out.println(rs.getInt("account_id"));
+                // System.out.println(rs.getString("account_id"));
+                return rs.getString("password");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    // ================Message vvvvvvvvv ========================
+    private int findIDMessage(Message message) {
+        try(Connection conn = ConnectionUtil.getConnection()){
+            PreparedStatement ps = conn.prepareStatement("select message_id from message where posted_by=? and time_posted_epoch=?;");
+            ps.setInt(1, message.getPosted_by());
+            ps.setLong(2, message.getTime_posted_epoch());
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                // System.out.println(rs.getString("account_id"));
+                return rs.getInt("message_id");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+    private boolean MessengerExists(int posted_by) {
+        try(Connection conn = ConnectionUtil.getConnection()){
+            PreparedStatement ps = conn.prepareStatement("select * from message where posted_by = ?;");
+            ps.setInt(1, posted_by);  
+            ResultSet rs = ps.executeQuery();
+            // System.out.println("checker: "+rs);
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    // view all values in database
+    private void viewDatabase() {
+        try(Connection conn = ConnectionUtil.getConnection()){
+            PreparedStatement ps = conn.prepareStatement("select * from message;");
+            // ps.setString(1, user);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                System.out.println("in ViewDB, msgID: "+rs.getInt("message_id")+
+                " postedBy: "+rs.getInt("posted_by")+
+                " text: "+rs.getString("message_text"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
